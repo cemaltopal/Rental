40292ad89454754b5fbcbfddff9f16e8f07e28dc
cemaltopal <cemaltopal25@gmail.com>
2025-12-09 23:22:13 +0300

    09/12

--- screens/CarsScreen.js
+++ screens/CarsScreen.js
@@ -2,11 +2,15 @@ import { StyleSheet, Text, View } from 'react-native'
 import {Card, IconButton, Paragraph, Title} from 'react-native-paper';
 const CarsScreen = () => {
   return (
-    <Card>
+    <Card style= {styles.card}>
       <Card.Cover source={require('../assets/cars/alfaromeo.jpg')} />
-      <Card.Content>
-        <Title>Cars</Title>
-        <Paragraph>Find your car</Paragraph>
+      <Card.Content
+        style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View>
+        <Title>Alfa Romeo</Title>
+        <Paragraph>From $20 / hour</Paragraph>
+        </View>
+        <IconButton icon="chevron-right" color="green" size={40} style={{backgroundColor: "green", color: "white"}}/>
       </Card.Content>
       </Card>
   )
@@ -14,4 +18,8 @@ const CarsScreen = () => {
 
 export default CarsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  card: {
+    margin: 15,
+  }
+})
