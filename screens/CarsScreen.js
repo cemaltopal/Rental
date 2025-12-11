bb08c36ebb1272875fa0a4919a3a96f870084d1c
cemaltopal <cemaltopal25@gmail.com>
2025-12-11 22:54:01 +0300

    11/12

--- screens/CarsScreen.js
+++ screens/CarsScreen.js
@@ -1,16 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import {Card, IconButton, Paragraph, Title} from 'react-native-paper';
+import colors from '../constants/colors';
 const CarsScreen = () => {
   return (
     <Card style= {styles.card}>
       <Card.Cover source={require('../assets/cars/alfaromeo.jpg')} />
       <Card.Content
-        style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        style={styles.cardContent}>
         <View>
-        <Title>Alfa Romeo</Title>
-        <Paragraph>From $20 / hour</Paragraph>
+        <Title style={styles.title}>Alfa Romeo</Title>
+        <Paragraph style={styles.parahraph}>From $20 / hour</Paragraph>
         </View>
-        <IconButton icon="chevron-right" color="green" size={40} style={{backgroundColor: "green", color: "white"}}/>
+        <IconButton icon="chevron-right" iconColor= "white" color="green" size={40} style={{backgroundColor: colors.color1}}/>
       </Card.Content>
       </Card>
   )
@@ -21,5 +22,22 @@ export default CarsScreen
 const styles = StyleSheet.create({
   card: {
     margin: 15,
+    borderRadius: 30,
+  },
+  cardContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 5
+
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  parahraph: {
+    color: colors.color1,
+    fontStyle: 'italic',
+
   }
 })
--- screens/navigation/HomeStack.js
+++ screens/navigation/HomeStack.js
@@ -3,14 +3,28 @@ import { StyleSheet } from 'react-native';
 import CarsDetailsScreen from '../CarsDetailsScreen';
 import CarsScreen from '../CarsScreen';
 import ReservationResultScreen from '../ReservationResultScreen';
+import colors from '../../constants/colors';
 
 const HomeStack = () => {
     const Stack = createNativeStackNavigator();
 
   return (
       <Stack.Navigator>
-        <Stack.Screen name="CarsScreen" component={CarsScreen} />
-        <Stack.Screen name="CarsDetailsScreen" component={CarsDetailsScreen} />
+        <Stack.Screen 
+          name="CarsScreen" 
+          component={CarsScreen} 
+          options={{
+              title: "Select a Car", 
+              headerStyle: {backgraundColor: colors},
+              headerTintColor: colors.color1}} />
+        <Stack.Screen 
+        name="CarsDetailsScreen" 
+        component={CarsDetailsScreen}
+        options={{
+          title: "Car Details",
+          headerStyle: {backgraundColor: colors},
+          headerTintColor: colors.color1
+        }} />
         <Stack.Screen name="ReservationResultScreen" component={ReservationResultScreen} />
       
     
